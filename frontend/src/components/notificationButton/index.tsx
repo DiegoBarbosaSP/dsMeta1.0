fdccd03c0fc0a17734e6010a8c0fae3242367759
DiegoBarbosaSP <diego8mile@hotmail.com>
2022-09-06 18:29:35 -0300

    first component

---
+++ frontend/src/components/notificationButton/index.tsx
@@ -0,0 +1,13 @@
+import vector from '../../assets/img/Vector.svg'
+import './style.css'
+
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={vector} alt="Notificar" />
+        </div>
+    )
+
+}
+export default NotificationButton
